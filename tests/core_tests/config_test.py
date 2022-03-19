b4192c9cae64ded205fadd3643ccce0d8f9c2b2e
Yolanda Wang <1621wyf@gmail.com>
2022-03-19 17:02:41 -0400

    Added default check.

--- tests/core_tests/config_test.py
+++ tests/core_tests/config_test.py
@@ -38,6 +38,14 @@ class ConfigTestCases(unittest.TestCase):
             "velocity": 100.0,
             "quad_rate": 100.0,
         }    
+    ic_default_check = {
+            "pos_x": 0.0,
+            "pos_y": 0.0,
+            "pos_z": 0.0,
+            "pos_ang": 0.0,
+            "velocity": 0.0,
+            "quad_rate": 0.0,
+        }
 
 
     def setup_helper (self, param, ic, ic_check):
@@ -46,6 +54,7 @@ class ConfigTestCases(unittest.TestCase):
     def test_init(self):
         self.setup_helper (self.dummy_param, self.ic_pos, self.ic_pos_check)
         self.setup_helper (self.dummy_param, self.ic_all, self.ic_all_check)
+        self.setup_helper (self.dummy_param, {}, self.ic_default_check)
 
 if __name__ == "__main__":
     unittest.main()
