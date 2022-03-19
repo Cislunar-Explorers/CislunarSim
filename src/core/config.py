05adcb8131ddc7565918c1dded8cb806caaf2266
Yolanda Wang <1621wyf@gmail.com>
2022-03-19 16:59:36 -0400

    Initialize test cases.

--- src/core/config.py
+++ src/core/config.py
@@ -1,30 +1,30 @@
-
 """Configurations of parameters and initial conditions for a given simulation."""
 
 from typing import Dict, Optional
 from dataclasses import dataclass
 
 import parameters
-import state
 
-@dataclass (frozen = True)
+
+@dataclass(frozen=True)
 class Config:
-    """Representation of the parameters and initial conditions of the simulation. This module depends on parameters.py. The variation in performance of different runs of the simulation depends on the variation of config.""" 
+    """Representation of the parameters and initial conditions of the simulation. This module depends on parameters.py. The variation in performance of different runs of the simulation depends on the variation of config."""
 
-    def _init_(self, parameters : parameters.Parameters, initial_conditions : Dict):
+    def _init_(
+        self, parameters: parameters.Parameters, initial_conditions: Optional[Dict] = {}
+    ):
         self.param = parameters
 
         default_conditions = {
-        "pos_x": 0.0,
-        "pos_y": 0.0,
-        "pos_z": 0.0,
-        "pos_ang": 0.0,
-        "velocity": 0.0,
-        "quad_rate": 0.0
+            "pos_x": 0.0,
+            "pos_y": 0.0,
+            "pos_z": 0.0,
+            "pos_ang": 0.0,
+            "velocity": 0.0,
+            "quad_rate": 0.0,
         }
 
         for key, value in initial_conditions.items():
             if key in default_conditions.keys():
                 setattr(default_conditions, key, value)
         self.init_cond = default_conditions
-
--- tests/core_tests/config_test.py
+++ tests/core_tests/config_test.py
@@ -1,13 +1,51 @@
 
 import unittest, logging
-import src.core.config
+from src.core.config import Config
+from src.core.parameters import Parameters
+# import core.config, src.core.parameters
 
 DEBUG = False
 
 class ConfigTestCases(unittest.TestCase):
-    pass
-    # TODO
+    '''This test tests whether '''
+    dummy_param = Parameters()
+    ic_pos = {
+        "pos_x": 3.0,
+            "pos_y": 3.0,
+            "pos_z": 3.0,
+    }
+    ic_pos_check = {
+            "pos_x": 3.0,
+            "pos_y": 3.0,
+            "pos_z": 3.0,
+            "pos_ang": 0.0,
+            "velocity": 0.0,
+            "quad_rate": 0.0,
+        }
+    ic_all = {
+            "pos_x": 10.0,
+            "pos_y": 10.0,
+            "pos_z": 10.0,
+            "pos_ang": 100.0,
+            "velocity": 100.0,
+            "quad_rate": 100.0,
+        }
+    ic_all_check = {
+            "pos_x": 10.0,
+            "pos_y": 10.0,
+            "pos_z": 10.0,
+            "pos_ang": 100.0,
+            "velocity": 100.0,
+            "quad_rate": 100.0,
+        }    
 
+
+    def setup_helper (self, param, ic, ic_check):
+        self.assert_equals(Config(param,ic).init_cond,ic_check)
+
+    def test_init(self):
+        self.setup_helper (self.dummy_param, self.ic_pos, self.ic_pos_check)
+        self.setup_helper (self.dummy_param, self.ic_all, self.ic_all_check)
 
 if __name__ == "__main__":
     unittest.main()
