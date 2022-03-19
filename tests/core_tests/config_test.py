d63c8c94c5e907a27415ad24a8b94a95e5aaa4c0
Yolanda Wang <1621wyf@gmail.com>
2022-03-19 16:07:02 -0400

    Set up Test Folder.

---
+++ tests/core_tests/config_test.py
@@ -0,0 +1,13 @@
+
+import unittest, logging
+import src.core.config
+
+DEBUG = False
+
+class ConfigTestCases(unittest.TestCase):
+    pass
+    # TODO
+
+
+if __name__ == "__main__":
+    unittest.main()
