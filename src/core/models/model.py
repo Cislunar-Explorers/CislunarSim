c8d708502fad3627d216ae00ff3e9e1d35ffdd01
tmf97 <tmf97@cornell.edu>
2022-03-19 14:26:20 -0400

    implement abstract model class

---
+++ src/core/models/model.py
@@ -0,0 +1,40 @@
+from abc import ABC, abstractmethod
+from typing import Dict, Any
+from core.state import State
+from core.parameters import Parameters
+
+
+class Model(ABC):
+    def __init__(self, parameters: Parameters) -> None:
+        self._parameters = parameters
+
+    def evaluate(self, state: State) -> Dict[str, Any]:
+        # TODO: wrap d_state with something (error checking?)
+        delta_state = self.d_state(state)
+        return delta_state
+
+    @abstractmethod
+    def d_state(self, state: State)  -> Dict[str, Any]:
+        ...
+
+class SensorModel(Model):
+    def __init__(self, parameters: Parameters) -> None:
+        super().__init__(parameters)
+
+    def evaluate(self, state: State) -> Dict[str, Any]:
+        return super().evaluate(state)
+
+    def d_state(self, state: State) -> Dict[str, Any]:
+        pass
+
+
+class ActuatorModel(Model):
+    def __init__(self, parameters: Parameters) -> None:
+        super().__init__(parameters)
+
+    def evaluate(self, state: State) -> Dict[str, Any]:
+        return super().evaluate(state)
+
+    def d_state(self, state: State) -> Dict[str, Any]:
+        pass
+
