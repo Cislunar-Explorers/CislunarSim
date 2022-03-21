9abd1940050a56a9a0efac7fe54320b10dff6c67
Cameron Goddard <camerongoddard@me.com>
2022-03-21 00:55:34 -0400

    add gyro model subclass

---
+++ src/core/models/gyro_model.py
@@ -0,0 +1,17 @@
+from core.models.model import SensorModel
+import numpy as np
+
+class GyroModel(SensorModel):
+    def __init__(self, parameters: Parameters) -> None:
+        super().__init__(parameters)
+    
+    def evaluate(self, state: State) -> Dict[str, Any]:
+        return super().evaluate(state)
+    
+    def d_state(self, state: State) -> Dict[str, Any]:
+        ang_vel_i = np.array([state.ang_vel_x, state.ang_vel_y, state.ang_vel_z])
+        gyro_bias = np.array(self._parameters.gyro_bias)
+
+        ang_vel_d = ang_vel_i + gyro_bias
+        ang_vel_d = np.random.normal(loc=ang_vel_d, scale=self._parameters.gyro_noise, size=3)
+        return {"ang_vel.x": ang_vel_d[0], "ang_vel.y": ang_vel_d[1], "ang_vel.z": ang_vel_d[2]} #return as quaternion rate?
