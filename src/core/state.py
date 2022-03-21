80c09f867a6ee0017f4c32cb606049994639393e
Cameron Goddard <camerongoddard@me.com>
2022-03-21 18:39:50 -0400

    add initial state structure

---
+++ src/core/state.py
@@ -0,0 +1,14 @@
+#from gnc import GNC
+#from device import Device
+
+state = {
+    "time" : 0,
+    "ang_vel_x" : 0.0,
+    "ang_vel_y" : 0.0,
+    "ang_vel_z" : 0.0,
+    "gnc_pos_x" : 0.0,
+    "gnc_pos_y" : 0.0,
+    "gnc_pos_z" : 0.0,
+    "device_gom_on" : True,
+    "device_gom_funct" : True
+}
