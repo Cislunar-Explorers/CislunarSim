9423f58644de817abbbf760eb4e7f3e09b2980c1
axc2@cornell.edu <andrew032012@gmail.com>
2022-03-25 18:24:18 -0400

    Implement test suite for state

--- src/core/state.py
+++ src/core/state.py
@@ -8,27 +8,27 @@ class State:
     """
 
     def __init__(self, state_dict: Dict = {}):
-        self.time = 0
+        self.time = 0.0
 
         # primitive state
 
-        # angular velocity
+        # angular velocity (radians/second)
         self.ang_vel_x = 0.0
         self.ang_vel_y = 0.0
         self.ang_vel_z = 0.0
 
-        # angular position (in quaternions)
+        # angular position
         self.gnc_pos_q1 = 0.0
         self.gnc_pos_q2 = 0.0
         self.gnc_pos_q3 = 0.0
         self.gnc_pos_q4 = 0.0
 
-        # velocity
+        # velocity (meters / second)
         self.vel_x = 0.0
         self.vel_y = 0.0
         self.vel_z = 0.0
 
-        # position
+        # position (meters)
         self.x = 0.0
         self.y = 0.0
         self.z = 0.0
@@ -36,19 +36,25 @@ class State:
         self.force_propulsion_thrusters = 0.0
         self.fuel_mass = 0.0
 
-        # derived state
+        # derived state (Newtons)
         self.force_earth = 0.0
         self.force_moon = 0.0
 
         # discrete state
-        self.propulsion_on = True
-        self.solenoid_actuation_on = True
+        self.propulsion_on = False
+        self.solenoid_actuation_on = False
 
         for key, value in state_dict.items():
             if key in self.__dict__.keys():
                 setattr(self, key, value)
 
     def to_array(self):
+        """
+        to_array() is the representation of the values of the fields as an array.
+
+        Returns:
+            Numpy array: contains all values stored in the fields.
+        """
         return np.array(list(self.__dict__.values()))
 
 
--- tests/core_tests/state_test.py
+++ tests/core_tests/state_test.py
@@ -1,14 +1,127 @@
 import unittest, logging
-import src.core.state
+from src.core.state import State
+
+import numpy as np
 
 DEBUG = False
 
 
 class StateTestCases(unittest.TestCase):
-    def test_state(self):
-        pass
+    def test_state_fields(self):
+        s_0 = {
+            "time": 0.0,
+            "ang_vel_x": 0.0,
+            "ang_vel_y": 0.0,
+            "ang_vel_z": 0.0,
+            "gnc_pos_q1": 0.0,
+            "gnc_pos_q2": 0.0,
+            "gnc_pos_q3": 0.0,
+            "gnc_pos_q4": 0.0,
+            "vel_x": 0.0,
+            "vel_y": 0.0,
+            "vel_z": 0.0,
+            "x": 0.0,
+            "y": 0.0,
+            "z": 0.0,
+            "force_propulsion_thrusters": 0.0,
+            "fuel_mass": 0.0,
+            "force_earth": 0.0,
+            "force_moon": 0.0,
+            "propulsion_on": False,
+            "solenoid_actuation_on": False,
+        }
 
-    # TODO
+        bool_fields = ["propulsion_on", "solenoid_actuation_on"]
+
+        for field in s_0.keys():
+            dummy_data = 1.0
+            if field in bool_fields:
+                dummy_data = True
+            s_copy = dict(s_0)
+            s_copy[field] = dummy_data
+            self.assert_equals(
+                s_copy,
+                State({field: dummy_data}).__dict__,
+            )
+
+        self.assert_equals(
+            {
+                "time": 0.0,
+                "ang_vel_x": 0.0,
+                "ang_vel_y": 0.0,
+                "ang_vel_z": 0.0,
+                "gnc_pos_q1": 0.0,
+                "gnc_pos_q2": 0.0,
+                "gnc_pos_q3": 0.0,
+                "gnc_pos_q4": 0.0,
+                "vel_x": 0.0,
+                "vel_y": 0.0,
+                "vel_z": 0.0,
+                "x": 0.0,
+                "y": 0.0,
+                "z": 0.0,
+                "force_propulsion_thrusters": 0.0,
+                "fuel_mass": 0.0,
+                "force_earth": 0.0,
+                "force_moon": 0.0,
+                "propulsion_on": False,
+                "solenoid_actuation_on": False,
+            },
+            State({}).__dict__,
+        )
+
+    def test_to_array(self):
+
+        self.assert_equals(
+            np.array(
+                [
+                    1.0,
+                    2.0,
+                    3.0,
+                    4.0,
+                    5.0,
+                    6.0,
+                    7.0,
+                    8.0,
+                    9.0,
+                    10.0,
+                    11.0,
+                    12.0,
+                    13.0,
+                    14.0,
+                    15.0,
+                    16.0,
+                    17.0,
+                    18.0,
+                    True,
+                    False,
+                ]
+            ),
+            State(
+                {
+                    "time": 1.0,
+                    "ang_vel_x": 2.0,
+                    "ang_vel_y": 3.0,
+                    "ang_vel_z": 4.0,
+                    "gnc_pos_q1": 5.0,
+                    "gnc_pos_q2": 6.0,
+                    "gnc_pos_q3": 7.0,
+                    "gnc_pos_q4": 8.0,
+                    "vel_x": 9.0,
+                    "vel_y": 10.0,
+                    "vel_z": 11.0,
+                    "x": 12.0,
+                    "y": 13.0,
+                    "z": 14.0,
+                    "force_propulsion_thrusters": 15.0,
+                    "fuel_mass": 16.0,
+                    "force_earth": 17.0,
+                    "force_moon": 18.0,
+                    "propulsion_on": True,
+                    "solenoid_actuation_on": False,
+                }
+            ).to_array(),
+        )
 
 
 if __name__ == "__main__":
