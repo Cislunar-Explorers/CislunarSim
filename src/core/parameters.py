4fce62e1a96318f32287b67ca692b4bee783893f
Cameron Goddard <47681797+cameron-goddard@users.noreply.github.com>
2022-03-26 16:35:54 -0400

    Gyro model (#9)
    * add gyro model subclass
    
    * update gyro bias and gyro noise parameters
    
    * add initial state structure
    
    * add documentation link and brief class overview
    
    * add quantization of gyro values
    
    * fix sensitivity unit error in parameters, typo in gyro_model
    
    * fix unit error
    
    * temp
    
    * add DS_Stores to gitignore
    
    * Fix some bugs related to imports
    
    * Add clean and noisy/biased tests
    
    * add comments
    
    * add docstrings
    
    * update parameters_test.py with new default values

--- src/core/parameters.py
+++ src/core/parameters.py
@@ -3,13 +3,15 @@ Sim Parameters Spreadsheet: https://cornell.box.com/s/z20wbp66q0pseqievmadf515uc
 """
 
 
+import math
 from typing import Dict
 
 
 class Parameters:
     def __init__(self, param_dict: Dict = {}):
-        self.gyro_bias = 0
-        self.gyro_noise = 0
+        self.gyro_bias = [0.497625, -0.10821875, 0.77490625]
+        self.gyro_noise = [0.1824535, 0.11738579, 0.19192256]
+        self.gyro_sensitivity = 0.015625*(math.pi/180)
         self.dry_mass = 0
         self.com = 0
         self.tank_volume = 0
