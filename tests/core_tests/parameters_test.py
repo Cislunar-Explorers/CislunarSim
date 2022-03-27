345fdadcdc4d7fcc7b32ce569051e1da44fb6ae3
axc2@cornell.edu <andrew032012@gmail.com>
2022-03-26 23:25:56 -0400

    Delete outdated tests

--- tests/core_tests/parameters_test.py
+++ tests/core_tests/parameters_test.py
@@ -25,7 +25,7 @@ class ParametersTestCase(unittest.TestCase):
             "gyro_noise": 0,
             "gyro_bias": [0.497625, -0.10821875, 0.77490625],
             "gyro_noise": [0.1824535, 0.11738579, 0.19192256],
-            "gyro_sensitivity": 0.015625*(math.pi/180),
+            "gyro_sensitivity": 0.015625 * (math.pi / 180),
             "dry_mass": 0,
             "com": 0,
             "tank_volume": 0,
@@ -70,52 +70,7 @@ class ParametersTestCase(unittest.TestCase):
             {
                 "gyro_bias": [0.497625, -0.10821875, 0.77490625],
                 "gyro_noise": [0.1824535, 0.11738579, 0.19192256],
-                "gyro_sensitivity": 0.015625*(math.pi/180),
-                "dry_mass": 0,
-                "com": 0,
-                "tank_volume": 0,
-                "thruster_force": 0,
-            },
-            Parameters({}).__dict__,
-        )
-        self.assertEqual(
-            d_main,
-            Parameters({"gyro_bias": 1}).__dict__,
-        )
-        d_main["gyro_bias"] = 0
-        d_main["gyro_noise"] = 2
-        self.assertEqual(
-            d_main,
-            Parameters({"gyro_noise": 2}).__dict__,
-        )
-        d_main["gyro_noise"] = 0
-        d_main["dry_mass"] = 3
-        self.assertEqual(
-            d_main,
-            Parameters({"dry_mass": 3}).__dict__,
-        )
-        d_main["dry_mass"] = 0
-        d_main["com"] = 4
-        self.assertEqual(
-            d_main,
-            Parameters({"com": 4}).__dict__,
-        )
-        d_main["com"] = 0
-        d_main["tank_volume"] = 5
-        self.assertEqual(
-            d_main,
-            Parameters({"tank_volume": 5}).__dict__,
-        )
-        d_main["tank_volume"] = 0
-        d_main["thruster_force"] = 6
-        self.assertEqual(
-            d_main,
-            Parameters({"thruster_force": 6}).__dict__,
-        )
-        self.assertEqual(
-            {
-                "gyro_bias": 0,
-                "gyro_noise": 0,
+                "gyro_sensitivity": 0.015625 * (math.pi / 180),
                 "dry_mass": 0,
                 "com": 0,
                 "tank_volume": 0,
