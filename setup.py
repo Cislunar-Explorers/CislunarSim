22c0bd2bf3af347f7634068cb164b5963a94f2a9
tmf97 <tmf97@cornell.edu>
2022-03-30 18:23:11 -0400

    git add sim_test and setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="cislunar-sim",
     version="0.1",
     author="SSDS",
+    author_email="tmf97@cornell.edu",
     description="HOOTL simulator for the Cislunar Explorers missions",
     install_requires=["numpy", "matplotlib", "pytest", "scipy"],
     package_dir={"": "src"},
