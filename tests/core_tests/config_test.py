17b47a535899a5642c59794858d686b91d6a6938
Yolanda Wang <1621wyf@gmail.com>
2022-04-03 17:48:10 -0400

    Oneline Fix for Pyright.

--- tests/core_tests/config_test.py
+++ tests/core_tests/config_test.py
@@ -55,7 +55,7 @@ class ConfigTestCases(unittest.TestCase):
         test_config_1 = Config(self.param_pos, self.ic_pos)
         test_config_2 = Config(self.param_all, self.ic_all)
         with self.assertRaises(MutationException):
-            test_config_1.init_cond = State(
+            test_config_1.init_cond = StateTime(
                 {"throw": "error"}
             )  # mutating init_cond would throw error
         with self.assertRaises(MutationException):
