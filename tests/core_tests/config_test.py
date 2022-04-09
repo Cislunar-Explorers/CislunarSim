002dd5544098e1ceeea1e94d56b45368fbe8ffa1
tmf97 <tmf97@cornell.edu>
2022-04-09 09:29:48 -0400

    fix pyright error in config_test

--- tests/core_tests/config_test.py
+++ tests/core_tests/config_test.py
@@ -56,7 +56,7 @@ class ConfigTestCases(unittest.TestCase):
         test_config_2 = Config(self.param_all, self.ic_all)
         with self.assertRaises(MutationException):
             test_config_1.init_cond = StateTime(
-                State(**{"throw": "error"})
+                State(**{"x": "error"})
             )  # mutating init_cond would throw error
         with self.assertRaises(MutationException):
             test_config_2.param = Parameters({})  # mutating param would throw error
