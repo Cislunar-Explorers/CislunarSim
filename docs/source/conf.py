fd5ef5a3628f5c60716d8eb0b001f4bbe72b5323
sjz38 <sjz38@cornell.edu>
2022-04-10 12:39:25 -0400

    change paths

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,8 +17,9 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(os.path.join("..", "..", "src")))
-# sys.path.insert(0, os.path.abspath('..'))
+# sys.path.insert(0, os.path.abspath(os.path.join("..", "..", "src")))
+sys.path.insert(0, os.path.abspath(os.path.join("..", "src")))
+sys.path.insert(0, os.path.abspath('..'))
 
 #
 
