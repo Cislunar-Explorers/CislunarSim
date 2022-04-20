89b964cb7394eec38200c18365098c4f1f16f449
axc2@cornell.edu <andrew032012@gmail.com>
2022-04-19 21:00:20 -0400

    fix formatting, remove unused import

--- src/core/sim.py
+++ src/core/sim.py
@@ -7,7 +7,6 @@ from utils.astropy_util import get_body_position
 from utils.log import log
 from utils.numbers import R_EARTH, R_MOON
 import matplotlib.pyplot as plt
-from mpl_toolkits import mplot3d
 from utils.constants import BodyEnum
 
 
@@ -26,20 +25,18 @@ class CislunarSim:
         self.num_iters = 0
 
         fig = plt.figure()
-        self.ax = fig.add_subplot( 111, projection="3d" )
+        self.ax = fig.add_subplot(111, projection="3d")
         self.ax.set_xlim(-10000000, 10000000)
         self.ax.set_ylim(-10000000, 10000000)
         self.ax.set_zlim(-10000000, 10000000)
         self.ax.set_xlabel("x")
         self.ax.set_ylabel("y")
         self.ax.set_zlabel("z")
-        #self.ax = plt.axes(projection='3d')
+        # self.ax = plt.axes(projection='3d')
         self.xlocs = np.array([])
         self.ylocs = np.array([])
         self.zlocs = np.array([])
-        
-        
-        
+
         self.ax.set
 
     def step(self) -> PropagatedOutput:
@@ -87,7 +84,7 @@ class CislunarSim:
         """
 
         state = self.state.state
-        
+
         if not np.isfinite(state.to_array()).all():
             # Thank you: https://stackoverflow.com/questions/911871/
             log.error("Stopping sim because of infinite value in state")
@@ -98,24 +95,23 @@ class CislunarSim:
             log.error("Stopping sim because it's running too long")
             return True
 
-        if (state.x**2 + state.y**2 + state.z**2)**0.5 < R_EARTH:
+        if (state.x**2 + state.y**2 + state.z**2) ** 0.5 < R_EARTH:
             log.error("Stopping sim because craft is inside the Earth")
             log.debug(f"r={(state.x**2 + state.y**2 + state.z**2)**0.5} < {R_EARTH}")
-            self.ax.scatter3D(self.xlocs, self.ylocs, self.zlocs, cmap='Greens')
-            print(self.xlocs)
-            u, v = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]
-            earth_x = R_EARTH*np.cos(u)*np.sin(v)
-            earth_y = R_EARTH*np.sin(u)*np.sin(v)
-            earth_z = R_EARTH*np.cos(v)
+            self.ax.scatter3D(self.xlocs, self.ylocs, self.zlocs, cmap="Greens")
+            u, v = np.mgrid[0 : 2 * np.pi : 20j, 0 : np.pi : 10j]
+            earth_x = R_EARTH * np.cos(u) * np.sin(v)
+            earth_y = R_EARTH * np.sin(u) * np.sin(v)
+            earth_z = R_EARTH * np.cos(v)
             self.ax.plot_surface(earth_x, earth_y, earth_z, color="g")
 
-            moon_cx, moon_cy, moon_cz = get_body_position(self.state.time, BodyEnum.Moon)
-            moon_x = moon_cx + R_MOON*np.cos(u)*np.sin(v)
-            moon_y = moon_cy + R_MOON*np.sin(u)*np.sin(v)
-            moon_z = moon_cz + R_MOON*np.cos(v)
+            moon_cx, moon_cy, moon_cz = get_body_position(
+                self.state.time, BodyEnum.Moon
+            )
+            moon_x = moon_cx + R_MOON * np.cos(u) * np.sin(v)
+            moon_y = moon_cy + R_MOON * np.sin(u) * np.sin(v)
+            moon_z = moon_cz + R_MOON * np.cos(v)
             self.ax.plot_surface(moon_x, moon_y, moon_z, color="g")
-
-
 
             plt.show()
             return True
