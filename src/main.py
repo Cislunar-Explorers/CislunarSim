c233e4f8eb308d13e2608fe379a41e8998f06f5c
axc2@cornell.edu <andrew032012@gmail.com>
2022-04-24 00:38:13 -0400

    remove outdated freefall function

--- src/main.py
+++ src/main.py
@@ -89,23 +89,6 @@ class SimRunner:
         return self.state_history
 
 
-def freefall():
-    # freefall from ~4000km altitude to test basic functionality of the sim
-    initial_condition = {
-        "x": 10_000_000,
-        "y": 1_000,
-        "z": 1_000,
-        "ang_vel_x": 4.5,
-        "time": 0.0,
-    }
-    models_to_use = [ModelEnum.PositionModel, ModelEnum.GyroModel]
-    conf = Config({}, initial_condition, models=models_to_use)
-
-    test_sim = SimRunner(conf)
-    data = test_sim.run()
-    df_to_csv(data)
-
-
 if __name__ == "__main__":
     data = SimRunner().run()
     df_to_csv(data)
