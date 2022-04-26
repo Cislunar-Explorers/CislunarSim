e3d0e14d6670470c1da9f29654584efc775049a8
Adam Nasir <adamnasir1220@gmail.com>
2022-04-25 21:43:59 -0400

    Fixed position model test

--- tests/core_tests/model_tests/position_dynamics_model_test.py
+++ tests/core_tests/model_tests/position_dynamics_model_test.py
@@ -1,5 +1,6 @@
+from email.mime import base
 import unittest
-from core.models.model_list import PositionDynamics
+from core.models.model_list import PositionDynamics, DerivedPosition
 from utils.test_utils import state_1, d3456
 
 
@@ -12,8 +13,12 @@ class PositionDynamicsModelTest(unittest.TestCase):
         """
         This function tests the position dynamics model's d_state function.
         """
+        base_state = state_1
         dummy_pd = PositionDynamics(d3456)
-        propagated_state = dummy_pd.d_state(1.0, state_1)
+        dummy_dpd = DerivedPosition(d3456)
+        base_state.derived_state.update(dummy_dpd.evaluate(1.0, state_1))
+        propagated_state = dummy_pd.d_state(1.0, base_state)
+
 
         # trivial tests that check to make sure the velocity components from the input to the output match exactly
         self.assertEqual(9.0, propagated_state["x"])
