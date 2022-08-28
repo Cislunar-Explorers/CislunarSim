34fa948440df17662fe47bc759642dd9289bae8a
Cameron Goddard <camerongoddard@me.com>
2022-08-28 14:18:32 -0400

    cleanup and document event class

--- src/core/event.py
+++ src/core/event.py
@@ -1,15 +1,24 @@
+from typing import Tuple
 from core.integrator.integrator import propagate_state
 from core.models.model_list import ModelContainer
 from core.state.state import State, ObservedState
-from core.state.statetime import PropagatedOutput, StateTime
+from core.state.statetime import StateTime
 
 class Event:
+    """Representation of a sim event with a list of models to be evaluated"""
 
-    #each event has only 1 state?
     def __init__(self, model_container: ModelContainer):
         self.model_container = model_container
     
-    def evaluate_model_list(self, state_time: StateTime) -> PropagatedOutput:
+    def evaluate_model_list(self, state_time: StateTime) -> Tuple[StateTime, ObservedState]:
+        """Evaluates all the models for this event
+
+        Args:
+            state_time (StateTime): The StateTime object at which this event takes place
+
+        Returns:
+            Tuple[StateTime, ObservedState]: The propagated statetime and observed state
+        """
 
         # Evaluate Actuator models to update state
         for actuator_model in self.model_container.actuator:
