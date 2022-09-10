b219699985ed1865d35f7a395113a9b433772181
axc2@cornell.edu <andrew032012@gmail.com>
2022-09-10 19:34:41 -0400

    initial commit

--- src/utils/matplotlib_util.py
+++ src/utils/matplotlib_util.py
@@ -2,20 +2,58 @@ import numpy as np
 import matplotlib.pyplot as plt
 from utils.astropy_util import get_body_position
 import matplotlib.animation as animation
-from utils.constants import BodyEnum, R_EARTH, R_MOON
+from utils.constants import D_T, BodyEnum, R_EARTH, R_MOON
 from datetime import datetime
+from matplotlib.widgets import Slider
+
 
 class Plot:
-
     def __init__(self, df):
         self.df = df
+
+        self.ts = df["true_state.time"].to_numpy()
+
+        self.xlocs = df["true_state.state.x"].to_numpy()
+        self.ylocs = df["true_state.state.y"].to_numpy()
+        self.zlocs = df["true_state.state.z"].to_numpy()
+        self.vel_xs = df["true_state.state.vel_x"].to_numpy()
+        self.vel_ys = df["true_state.state.vel_y"].to_numpy()
+        self.vel_zs = df["true_state.state.vel_z"].to_numpy()
+
+        self.locs = np.array([self.xlocs, self.ylocs, self.zlocs])
+        self.ang_x = df["true_state.state.ang_vel_x"].to_numpy()
+        self.ang_x_obs = df["observed_state.ang_vel_x"].to_numpy()
+
         self.fig_2d = plt.figure()
-        self.ax_vel = plt.subplot(221)
-        self.ax_pos = plt.subplot(222)
-        self.ax_ang_vel_x = plt.subplot(223)
+        self.ax_vel = plt.subplot(321)
+        self.ax_pos = plt.subplot(322)
+        self.ax_ang_vel_x = plt.subplot(323)
+
+        self.ax_vel.set_xlim(self.ax_vel.get_xlim())
+
+        axcolor = "lightgoldenrodyellow"
+        axfreq = plt.axes([0.1, 0.1, 0.5, 0.01], facecolor=axcolor)
+        self.t_max = Slider(
+            axfreq,
+            "t",
+            self.ts[0],
+            self.ts[-1],
+            valinit=self.ts[-1],
+            valstep=D_T,
+        )
+        self.t_max.on_changed(self.update)
+        self.sub_ts = np.copy(self.ts)
+        self.sub_xlocs = np.copy(self.xlocs)
+        self.sub_ylocs = np.copy(self.ylocs)
+        self.sub_zlocs = np.copy(self.zlocs)
+        self.sub_vel_xs = np.copy(self.vel_xs)
+        self.sub_vel_ys = np.copy(self.vel_ys)
+        self.sub_vel_zs = np.copy(self.vel_zs)
+        self.sub_ang_x = np.copy(self.ang_x)
+        self.sub_ang_x_obs = np.copy(self.ang_x_obs)
 
         self.fig_3d = plt.figure("CislunarSim")
-        self.ax = self.fig_3d.gca(projection='3d')
+        self.ax = self.fig_3d.gca(projection="3d")
 
         self.u = np.linspace(0, 2 * np.pi, 60)
         self.v = np.linspace(0, np.pi, 60)
@@ -33,21 +71,6 @@ class Plot:
         self.ax_pos.set_xlabel("t")
         self.ax_pos.set_ylabel("Position")
 
-        self.ts = df["true_state.time"].to_numpy()
-
-        self.xlocs = df["true_state.state.x"].to_numpy()
-        self.ylocs = df["true_state.state.y"].to_numpy()
-        self.zlocs = df["true_state.state.z"].to_numpy()
-        self.vel_xs = df["true_state.state.vel_x"].to_numpy()
-        self.vel_ys = df["true_state.state.vel_y"].to_numpy()
-        self.vel_zs = df["true_state.state.vel_z"].to_numpy()
-
-        self.times = df["true_state.time"].to_numpy()
-
-        self.locs = np.array([self.xlocs, self.ylocs, self.zlocs])
-        self.ang_x = df["true_state.state.ang_vel_x"].to_numpy()
-        self.ang_x_obs = df["observed_state.ang_vel_x"].to_numpy()
-
         self.annot = self.ax.annotate(
             "",
             xy=(0, 0),
@@ -62,21 +85,56 @@ class Plot:
         self.plot_data_2d()
         self.plot_data_3d()
         plt.tight_layout()
-        #plt.show()
+        # plt.show()
+
+    def update(self, _):
+        t_max_index = int(
+            (self.t_max.val - self.ts[0])
+            // ((self.ts[-1] - self.ts[0]) // len(self.ts))
+        )
+
+        print(self.t_max.val)
+        print(self.t_max.val - self.ts[0])
+        print((self.ts[-1] - self.ts[0]) // len(self.ts))
+        print(t_max_index)
+        print(len(self.ts))
+
+        self.sub_ts = self.ts[:t_max_index:]
+        self.sub_xlocs = self.xlocs[:t_max_index:]
+        self.sub_ylocs = self.ylocs[:t_max_index:]
+        self.sub_zlocs = self.zlocs[:t_max_index:]
+        self.sub_vel_xs = self.vel_xs[:t_max_index:]
+        self.sub_vel_ys = self.vel_ys[:t_max_index:]
+        self.sub_vel_zs = self.vel_zs[:t_max_index:]
+        self.sub_ang_x = self.ang_x[:t_max_index:]
+        self.sub_ang_x_obs = self.ang_x_obs[:t_max_index:]
+
+        print(self.sub_ts)
+        # l.set_ydata(amp*np.sin(2*np.pi*freq*t))
+        self.fig_2d.canvas.draw_idle()
+        self.fig_2d.canvas.flush_events()
+
+        self.ax_vel.clear()
+        self.ax_pos.clear()
+        self.ax_ang_vel_x.clear()
+        self.plot_data_2d()
 
     def plot_data_2d(self) -> None:
         """Procedure that displays 2d plots of spacecraft data"""
-        self.ax_vel.plot(self.ts, self.vel_xs, "--", c="hotpink", label="x")
-        self.ax_vel.plot(self.ts, self.vel_ys, "--", c="green", label="y")
-        self.ax_vel.plot(self.ts, self.vel_zs, "--", c="blue", label="z")
 
-        self.ax_pos.plot(self.ts, self.xlocs, "--", c="hotpink", label="x")
-        self.ax_pos.plot(self.ts, self.ylocs, "--", c="green", label="y")
-        self.ax_pos.plot(self.ts, self.zlocs, "--", c="blue", label="z")
+        self.ax_vel.plot(self.sub_ts, self.sub_vel_xs, "--", c="hotpink", label="x")
+        self.ax_vel.plot(self.sub_ts, self.sub_vel_ys, "--", c="green", label="y")
+        self.ax_vel.plot(self.sub_ts, self.sub_vel_zs, "--", c="blue", label="z")
 
-        self.ax_ang_vel_x.plot(self.ts, self.ang_x, "--", c="hotpink", label="x (true)")
+        self.ax_pos.plot(self.sub_ts, self.sub_xlocs, "--", c="hotpink", label="x")
+        self.ax_pos.plot(self.sub_ts, self.sub_ylocs, "--", c="green", label="y")
+        self.ax_pos.plot(self.sub_ts, self.sub_zlocs, "--", c="blue", label="z")
+
         self.ax_ang_vel_x.plot(
-            self.ts, self.ang_x_obs, "--", c="green", label="x (observed)"
+            self.sub_ts, self.sub_ang_x, "--", c="hotpink", label="x (true)"
+        )
+        self.ax_ang_vel_x.plot(
+            self.sub_ts, self.sub_ang_x_obs, "--", c="green", label="x (observed)"
         )
 
         self.ax_vel.legend()
@@ -89,7 +147,7 @@ class Plot:
 
         locs = np.array([self.xlocs, self.ylocs, self.zlocs])
         traj = plt.plot(self.xlocs, self.ylocs, self.zlocs, lw=2, c="blue")[0]
-        
+
         traj_ani = animation.FuncAnimation(
             self.fig_3d,
             self.animate_traj,
@@ -99,10 +157,10 @@ class Plot:
             blit=False,
         )
 
-        self.ax.set_box_aspect(aspect = (1,1,1))
+        self.ax.set_box_aspect(aspect=(1, 1, 1))
 
         # Calculation and plotting of earth's position
-       
+
         earth_x = R_EARTH * np.outer(np.cos(self.u), np.sin(self.v))
         earth_y = R_EARTH * np.outer(np.sin(self.u), np.sin(self.v))
         earth_z = R_EARTH * np.outer(np.ones(np.size(self.u)), np.cos(self.v))
@@ -118,24 +176,30 @@ class Plot:
         moon = [self.ax.plot_surface(moon_x, moon_y, moon_z, color="gray")]
 
         moon_ani = animation.FuncAnimation(
-            self.fig_3d,
-            self.animate_moon,
-            frames=len(self.xlocs),
-            fargs=(locs, moon)
+            self.fig_3d, self.animate_moon, frames=len(self.xlocs), fargs=(locs, moon)
         )
         plt.show()
 
     def animate_traj(self, num, locs, line):
-        self.ax.set_title('Apollo 12 SIVB \nTime = ' + datetime.utcfromtimestamp(self.times[num]).strftime('%Y-%m-%d %H:%M:%S'))
+        self.ax.set_title(
+            "Apollo 12 SIVB \nTime = "
+            + datetime.utcfromtimestamp(self.ts[num]).strftime("%Y-%m-%d %H:%M:%S")
+        )
         line.set_data(locs[0:2, :num])
         line.set_3d_properties(locs[2, :num])
         return line
 
     def animate_moon(self, num, locs, moon):
-        moon_cx = float(self.df["true_state.derived_state.r_mo"][num].strip("[]").split(" ")[1])
-        moon_cy = float(self.df["true_state.derived_state.r_mo"][num].strip("[]").split(" ")[2])
-        moon_cz = float(self.df["true_state.derived_state.r_mo"][num].strip("[]").split(" ")[3])
-        
+        moon_cx = float(
+            self.df["true_state.derived_state.r_mo"][num].strip("[]").split(" ")[1]
+        )
+        moon_cy = float(
+            self.df["true_state.derived_state.r_mo"][num].strip("[]").split(" ")[2]
+        )
+        moon_cz = float(
+            self.df["true_state.derived_state.r_mo"][num].strip("[]").split(" ")[3]
+        )
+
         moon_x = moon_cx + R_MOON * np.outer(np.cos(self.u), np.sin(self.v))
         moon_y = moon_cy + R_MOON * np.outer(np.sin(self.u), np.sin(self.v))
         moon_z = moon_cz + R_MOON * np.outer(np.ones(np.size(self.u)), np.cos(self.v))
