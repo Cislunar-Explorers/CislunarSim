0acc020ac4868c8f73cbae7cfd7cc368fc61dafa
Andrew Chen <andrew032012@gmail.com>
2022-11-01 15:43:09 -0400

    Add parens on lru_cache

--- src/utils/astropy_util.py
+++ src/utils/astropy_util.py
@@ -10,7 +10,7 @@ from astropy.coordinates import SkyCoord
 
 
 # Heavily reference the get_ephemeris function here: https://github.com/Cislunar-Explorers/FlightSoftware/blob/master/OpticalNavigation/core/observe_functions.py
-@lru_cache
+@lru_cache()
 def get_body_position(time: int, body: BodyEnum) -> Tuple[float, float, float]:
     """Gets position vector of [body] based on [time]
 
