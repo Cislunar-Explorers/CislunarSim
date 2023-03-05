7a6dfa1dc93ce2c179cb570599432a91d570b692
Thomas F. McFarland <tfm62@cornell.edu>
2023-03-04 19:14:22 -0500

    Misspelling :(

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     author="SSDS",
     author_email="tmf97@cornell.edu",
     description="HOOTL simulator for the Cislunar Explorers missions",
-    install_requires=["numpy", "matplotlib", "pytest", "scipy", "astropy", "pandas", "jsonschema", "python-dotenv", "sqlalchemy", "psutil", "bitstring","numba","numpy-quaternion","opencv-python-headless","opencv-python","pyquaternion","tqdm","uptime","parameterized","Adafruit-Blinka","adafruit-circuitpython-busdevic","adafruit-circuitpython-ds3231","adafruit-circuitpython-fxas21002c","adafruit-circuitpython-fxos8700","ADS1115","pigpio"],
+    install_requires=["numpy", "matplotlib", "pytest", "scipy", "astropy", "pandas", "jsonschema", "python-dotenv", "sqlalchemy", "psutil", "bitstring","numba","numpy-quaternion","opencv-python-headless","opencv-python","pyquaternion","tqdm","uptime","parameterized","Adafruit-Blinka","adafruit-circuitpython-busdevice","adafruit-circuitpython-ds3231","adafruit-circuitpython-fxas21002c","adafruit-circuitpython-fxos8700","ADS1115","pigpio"],
     package_dir={"": "src"},
 )
