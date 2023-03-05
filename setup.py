ad463857dec7e9014afe81a05ea0f38b93487cfc
Thomas F. McFarland <tfm62@cornell.edu>
2023-03-04 19:08:24 -0500

    Hopefully finish requirements

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     author="SSDS",
     author_email="tmf97@cornell.edu",
     description="HOOTL simulator for the Cislunar Explorers missions",
-    install_requires=["numpy", "matplotlib", "pytest", "scipy", "astropy", "pandas", "jsonschema", "python-dotenv", "sqlalchemy", "psutil"],
+    install_requires=["numpy", "matplotlib", "pytest", "scipy", "astropy", "pandas", "jsonschema", "python-dotenv", "sqlalchemy", "psutil", "bitstring","numba","numpy-quaternion","opencv-python-headless","opencv-python","pyquaternion","tqdm","uptime","parameterized"],
     package_dir={"": "src"},
 )
