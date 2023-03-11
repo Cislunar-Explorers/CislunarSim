65b71799db7cb483a9fee556986a7eb6dcf88d71
Thomas F. McFarland <tfm62@cornell.edu>
2023-03-11 15:04:27 -0500

    Revise tests to not also test the fsw, may want to revise this

--- tests/sim_test.py
+++ tests/sim_test.py
@@ -7,21 +7,25 @@ from utils.constants import ModelEnum, D_T
 
 class SimTest(unittest.TestCase):
     def test_init(self):
-        config = Config({}, {"time": time.time()}, models=[ModelEnum.UnittestModel])
-        sim = CislunarSim(config)
+        config = Config({}, {"time": time.time()},
+                        models=[ModelEnum.UnittestModel])
+        sim = CislunarSim(config, False)
         self.assertIsNotNone(sim)
 
     def test_step(self):
-        config = Config({}, {"time": time.time()}, models=[ModelEnum.UnittestModel])
-        sim = CislunarSim(config)
+        config = Config({}, {"time": time.time()},
+                        models=[ModelEnum.UnittestModel])
+        sim = CislunarSim(config, False)
 
         initial_conditions = sim._config.init_cond
         next_conditions = sim.step()
         # The unittest model keeps every state the same. dState/dt = 0
-        self.assertEqual(initial_conditions.state, next_conditions.true_state.state)
+        self.assertEqual(initial_conditions.state,
+                         next_conditions.true_state.state)
 
         # Verify that time incremented in the sim by the expected amount
-        self.assertEqual(next_conditions.true_state.time, sim._config.init_cond.time + D_T)
+        self.assertEqual(next_conditions.true_state.time,
+                         sim._config.init_cond.time + D_T)
 
 
 if __name__ == "__main__":
