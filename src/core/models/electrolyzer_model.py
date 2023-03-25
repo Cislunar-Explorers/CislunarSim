86e7492288b136fbeaaefeef88640e3c893f9d7f
Chuqi Yuan <chuqiyuan@dhcp-vl2041-18474.eduroam.cornell.edu>
2023-03-25 16:07:07 -0400

    altered electrolyzer_model to only output rates of electrolysis, rate of hydrogen gas production, and rate of oxygen gas production

--- src/core/models/electrolyzer_model.py
+++ src/core/models/electrolyzer_model.py
@@ -1,3 +1,4 @@
+#from src.core.models.model import ActuatorModel
 from core.models.model import ActuatorModel
 from core.parameters import Parameters
 from core.state.statetime import StateTime
@@ -9,10 +10,9 @@ from utils.log import log
 class ElectrolyzerModel(ActuatorModel):
   """Propagates fuel mass and chamber pressure according to electrolysis parameters"""
 
-  def __init__(self, duration: float, parameters: Parameters) -> None:
+  def __init__(self, parameters: Parameters) -> None:
       super().__init__(parameters)
 
-      self._duration = duration
       self._electrolyzer_rate = np.array(self._parameters.electolyzer_rate)
       # volume of combustion chamber
       self._chamber_vol = np.array(self._parameters.combustion_chamber_volume)
@@ -33,18 +33,26 @@ class ElectrolyzerModel(ActuatorModel):
       # get fuel mass at this time
       fuel_mass_i = np.array([state_time.state.fuel_mass])
       chamber_temp = np.array([state_time.state.chamber_temp])
-      kg_electrolyzed = np.array([state_time.state.kg_electrolyzed])
+      #kg_electrolyzed = np.array([state_time.state.kg_electrolyzed])
       
       # log the critical chamber temperature
       if (chamber_temp <= np.array(self._parameters.temperature_min) or 
           chamber_temp >= np.array(self._parameters.temperature_max)):
         log.critical("Chamber temp is out of range")
 
+    #non-constant electrolyzer rate
+
       # calculate updated fuel mass
-      kg_electrolyzed += self._duration * self._electrolyzer_rate
-      fuel_mass_d = fuel_mass_i - kg_electrolyzed
+      # state_time.state.kg_electrolyzed += self._duration * self._electrolyzer_rate
+      #fuel_mass_d = fuel_mass_i - kg_electrolyzed
+
+      moleWaterRate = self._electrolyzer_rate * (1/M_WATER)
+
+      h2Rate = moleWaterRate
+      o2Rate = moleWaterRate * 0.5
 
       # return updated fuel mass
       return {
-        "fuel_mass": fuel_mass_d
+        "Electrolyzer rate (kg/s)": self._electrolyzer_rate, "H2 Production Rate (Moles/s)": h2Rate, 
+        "O2 Production Rate (moles/s)": o2Rate
       }
